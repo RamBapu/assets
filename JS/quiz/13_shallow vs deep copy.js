664f36bc333e4860c3d8cbdb40214285a2cc9dd1
Ram Bapu <rambapu270197@gmail.com>
2026-04-25 16:29:08 +0530

    Enhance shallow vs deep copy example with detailed comments; add Redux documentation with comprehensive explanations and examples

--- JS/quiz/13_shallow vs deep copy.js
+++ JS/quiz/13_shallow vs deep copy.js
@@ -1,11 +1,13 @@
 let obj1 = { a: 1, b: { c: 2 } };
 let shallowCopy = Object.assign({}, obj1);
-obj1.b.c = 3;
-console.log(shallowCopy);
-console.log(obj1);
+obj1.b.c = 3; // Nested objects will have same obj reference
+console.log(shallowCopy); // { a: 1, b: { c: 3 } }
+console.log(obj1); // { a: 1, b: { c: 3 } }
+console.log(shallowCopy == obj1); // false - different object references
+console.log(shallowCopy === obj1); // false - different object references
 
 let obj2 = { a: 1, b: { c: 2 } };
 let deepCopy = JSON.parse(JSON.stringify(obj2));
 obj2.b.c = 3;
-console.log(deepCopy);
-console.log(obj2);
+console.log(deepCopy); // { a: 1, b: { c: 2 } }
+console.log(obj2); // { a: 1, b: { c: 3 } }
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Roadmap
+
+HTML, CSS, Agile
+JS, JS Quiz
+JS Practice
+React, React Quiz
+Redux
+Typescript
+NextJS Rendering
+Projects
+
+MERN - Node js
+NEXT - Next js
